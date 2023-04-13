c612690c5d8680f59defddea5b959da02a740f9a
Minji Lee <minji0479@gmail.com>
2023-04-13 22:01:37 +0900

    week1 end

--- Week1/2468.py
+++ Week1/2468.py
@@ -11,4 +11,32 @@ for i in range(0, num) :
     mx.append(max(li[i]))
     
 mxmx = max(mx)
+data = [[[0 for _ in range(num)] for _ in range(num)] for _ in range(mxmx)]
 
+for h in range(0,mxmx) :
+    for i in range(0,num) :
+        for j in range(0, num):
+            if li[i][j] <= h :
+                data[h][i][j] = 1
+            else :
+                data[h][i][j] = 0
+print(data)
+
+dx = [1,0,-1,0]
+dy = [0.1,0,-1]
+
+s = []
+output = []
+
+for i in range(mxmx) :
+    data[i][0][0] = 1
+    s.append([0,0])
+    
+    while len(s) != 0 :
+        count = 1
+        x = s[-1][0]
+        y = s[-1][1]
+        s.pop(-1)
+        
+        for j in range(0,4) :
+            if 
--- Week1/9663.py
+++ Week1/9663.py
@@ -2,7 +2,6 @@ count = 0
 num = int(input())
 li = [0] * num
 
-
 def bt(index) :
     global count, num, li
     if(index == num) :
@@ -15,13 +14,10 @@ def bt(index) :
             check = True
             for j in range(index):
                 if li[index] == li[j] or abs(index - j) == abs(li[index]- li[j]) :
-                # if li[index] == li[j] or abs(li[index]-li[j]) == abs(index - j):
                     check = False
                     break
             if check == True:
                 bt(index+1)
-            # else :
-            #     continue8
     
 bt(0)
 print(count)
