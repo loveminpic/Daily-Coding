c8967433a092605f3119b4d030c612b77e2eb6dd
Minji Lee <minji0479@gmail.com>
2023-04-29 00:32:18 +0900

    배낭 solved

--- Week4/12865.py
+++ Week4/12865.py
@@ -4,19 +4,18 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 arr = [0] * (K+1)
-
+product = []
 
 for i in range(0,N) :
-    W, V = map(int, input().split())
+    product.append(list(map(int, input().split())))
 
-    for j in range(1, W) :
-        if arr[j] > mx :
-            mx = arr[j]
-    arr[W] = mx
-
-for i in range(K, 1, -1) :
-    for j in range(1, i) :
-        if arr[i - j] + arr[j] > arr[i] :
-            arr[i] =  arr[i - j] + arr[j]
-               
+for i in range(0,N) :
+    w,v = product[i]
+    
+    for j in range(K, w - 1, -1) :
+        if arr[j-w] + v > arr[j] and i-j != j :
+             arr[j] = arr[j-w] + v
+        if j-w == j :
+            if arr[j-w] > v :
+                arr[w] = arr[j-w]     
 print(arr[K])
