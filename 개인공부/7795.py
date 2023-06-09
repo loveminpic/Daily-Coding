8854aed4b16e63f6d29fbb7ac2292aa3fcf766e5
Minji Lee <minji0479@gmail.com>
2023-06-09 09:39:37 +0900

    20230609

---
+++ 개인공부/7795.py
@@ -0,0 +1,31 @@
+import sys
+
+N =int(sys.stdin.readline().strip())
+
+for i in range(N) :
+    A, B = map(int, sys.stdin.readline().split())
+
+    a_list = list(map(int, sys.stdin.readline().split()))
+    b_list = list(map(int, sys.stdin.readline().split()))
+    count = 0
+    
+    a_list = sorted(a_list,reverse=True)
+    b_list = sorted(b_list)
+    
+    a_len = len(a_list)
+    b_len = len(b_list)
+    
+    for j in range(a_len) :
+        for g in range(b_len) :
+            if b_list[g] < a_list[j] :
+                count += 1
+            else :
+                break
+    
+    # for j in range(a_len) :
+    #     for g in range(b_len) :
+    #         if b_list[g] < a_list[j] :
+    #             count += b_len - g 
+    #             break
+
+    print(count)
